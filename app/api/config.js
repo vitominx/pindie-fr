d596ea28342d60a8b1c37bd1a11d1df64d22cdba
vitominx <149701427+vitominx@users.noreply.github.com>
2024-05-25 08:50:41 +0300

    <изменение кофига>

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3001/api";
+export const BASE_URL = "https://vitominx-backend.nomoredomainswork.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
